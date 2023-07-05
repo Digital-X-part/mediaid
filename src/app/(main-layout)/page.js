e34d094f65cc8f77429b8a8c6c32c21f353a9de0
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-07-05 14:48:07 +0600

    folder structure created

--- src/app/(main-layout)/page.js
+++ src/app/(main-layout)/page.js
@@ -1,9 +1,11 @@
+import Banner from '@/components/Pages/Home/Banner';
 import React from 'react';
 
 const HomePage = () => {
     return (
         <div>
             {/* all home page components will be place here */}
+            <Banner></Banner>
             this is home page
         </div>
     );
---
+++ src/components/Pages/Home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            this is banner
+        </div>
+    );
+};
+
+export default Banner;
