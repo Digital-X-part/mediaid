63299d3ed144a5a101215a0ab8845c531f288bcf
Eshrak <eshrakg62@gmail.com>
2023-07-06 01:45:20 +0600

    add a coursel slider

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    // Your existing Next.js configurations...
+  
+    // Add this configuration for serving static files from the public directory
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination: '/:path*',
+        },
+      ];
+    },
+  };
+  
--- src/components/Shared/Navbar/TopNavbar.jsx
+++ src/components/Shared/Navbar/TopNavbar.jsx
@@ -1,12 +1,6 @@
-import React from 'react';
 
 const TopNavbar = () => {
-    return (
-        <div>
-            This is top navbar
-            This is for testing my branch Eshrak
-        </div>
-    );
+
 };
 
 export default TopNavbar;
