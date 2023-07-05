7f49033dad3586e78831511ca929a2573915447a
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-07-05 18:34:27 +0600

    icon added on bottom navbar

--- src/components/Shared/Navbar/BottomNavbar.jsx
+++ src/components/Shared/Navbar/BottomNavbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BsHouseDoorFill, BsPerson, BsTagsFill, BsBagFill } from "react-icons/bs";
 
 const BottomNavbar = () => {
     return (
-        <div>
-            This is bottom navbar
+        <div className='w-full md:hidden py-2 px-5 bg-slate-300 fixed bottom-0 flex items-center justify-between'>
+            <div className='flex flex-col items-center justify-center'>
+                <BsHouseDoorFill className='w-5 h-5 text-black' />
+                <p className='text-xs font-medium'>Home</p>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+                <BsBagFill className='w-5 h-5 text-black' />
+                <p className='text-xs font-medium'>Shop</p>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+                <BsTagsFill className='w-5 h-5 text-black' />
+                <p className='text-xs font-medium'>Offer</p>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+                <BsPerson className='w-5 h-5 text-black' />
+                <p className='text-xs font-medium'>Account</p>
+            </div>
         </div>
     );
 };
