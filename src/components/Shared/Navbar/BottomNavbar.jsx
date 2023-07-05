09b8151ef48fe9941c7a77ce3410d9e038994d99
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-07-05 19:30:16 +0600

    bottom navbar added

--- src/components/Shared/Navbar/BottomNavbar.jsx
+++ src/components/Shared/Navbar/BottomNavbar.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import { BsHouseDoorFill, BsPerson, BsTagsFill, BsBagFill } from "react-icons/bs";
+import Link from "next/link";
+import React from "react";
+import {
+  BsHouseDoorFill,
+  BsPerson,
+  BsTagsFill,
+  BsBagFill,
+} from "react-icons/bs";
 
 const BottomNavbar = () => {
-    return (
-        <div className='w-full md:hidden py-2 px-5 bg-slate-300 fixed bottom-0 flex items-center justify-between'>
-            <div className='flex flex-col items-center justify-center'>
-                <BsHouseDoorFill className='w-5 h-5 text-black' />
-                <p className='text-xs font-medium'>Home</p>
-            </div>
-            <div className='flex flex-col items-center justify-center'>
-                <BsBagFill className='w-5 h-5 text-black' />
-                <p className='text-xs font-medium'>Shop</p>
-            </div>
-            <div className='flex flex-col items-center justify-center'>
-                <BsTagsFill className='w-5 h-5 text-black' />
-                <p className='text-xs font-medium'>Offer</p>
-            </div>
-            <div className='flex flex-col items-center justify-center'>
-                <BsPerson className='w-5 h-5 text-black' />
-                <p className='text-xs font-medium'>Account</p>
-            </div>
+  return (
+    <div className="w-full md:hidden py-2 px-5 sm:px-8 z-[100] bg-slate-200 fixed bottom-0 flex items-center justify-between">
+      <Link href="/">
+        <div className="flex flex-col items-center justify-center">
+          <BsHouseDoorFill className="w-5 h-5 text-black" />
+          <p className="text-xs font-medium">Home</p>
         </div>
-    );
+      </Link>
+      <Link href="/shop">
+        <div className="flex flex-col items-center justify-center">
+          <BsBagFill className="w-5 h-5 text-black" />
+          <p className="text-xs font-medium">Shop</p>
+        </div>
+      </Link>
+      <Link href="/offer">
+        <div className="flex flex-col items-center justify-center">
+          <BsTagsFill className="w-5 h-5 text-black" />
+          <p className="text-xs font-medium">Offer</p>
+        </div>
+      </Link>
+      <Link href="/account">
+        <div className="flex flex-col items-center justify-center">
+          <BsPerson className="w-5 h-5 text-black" />
+          <p className="text-xs font-medium">My Account</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default BottomNavbar;
