96765c51477abf2b5c976719e1d7d60973e23019
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-07-06 17:31:43 +0600

    create flash sale section

--- next.config.js
+++ next.config.js
@@ -5,9 +5,17 @@ const nextConfig = {
         {
           protocol: 'https',
           hostname: 'i.ibb.co',
+          
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          
         },
       ],
     },
+
+    
 }
 
 module.exports = nextConfig
--- src/components/Pages/Home/FlashSale/FlashSale.jsx
+++ src/components/Pages/Home/FlashSale/FlashSale.jsx
@@ -1,5 +1,11 @@
 "use client";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import { MdAddShoppingCart } from "react-icons/md";
 
 const FlashSale = () => {
   const [time, setTime] = useState({
@@ -12,7 +18,7 @@ const FlashSale = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date();
-      const targetDate = new Date("2023-07-07");
+      const targetDate = new Date("2023-07-09");
       const timeDiff = targetDate.getTime() - now.getTime();
       let days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
@@ -33,18 +39,233 @@ const FlashSale = () => {
   }, []);
 
   return (
-    <div className="px-1">
-      <div className="flex justify-between">
-        <h4 className="font-bold text-2xl">Flash Sale</h4>
-        <div className="flex text-center gap-1 font-mono text-lg font-bold text-yellow-500 border px-2 py-1 cursor-pointer">
+    <div className="px-1 my-8">
+      <div className="flex items-end justify-between">
+       <div className="flex gap-2">
+       <h4 className="font-bold text-2xl">Flash Sale</h4>
+        <div className="flex items-end text-center gap-1 font-mono text-lg font-bold text-yellow-500 cursor-pointer">
           <p>{time.days < 10 ? "0" + time.days : time.days}</p> <span>:</span>
           <p>{time.hours < 10 ? "0" + time.hours : time.hours}</p>{" "}
           <span>:</span>
           <p>{time.minutes < 10 ? "0" + time.minutes : time.minutes}</p>{" "}
           <span>:</span>
-          <p>{time.seconds < 10 ? "0" + time.seconds : time.seconds}</p> ➩
+          <p>{time.seconds < 10 ? "0" + time.seconds : time.seconds}</p> 
         </div>
+       </div>
+        <p className="text-sm tracking-wide mr-1">View All</p>
       </div>
+      <Swiper slidesPerView={2.5} spaceBetween={10} className="mySwiper my-1">
+        <SwiperSlide className="bg-white rounded-sm shadow-sm p-1 my-2 mx-1">
+          <Image
+            src="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+            alt=""
+            width={200}
+            height={200}
+            className="object-cover w-full h-32 rounded-sm"
+          />
+          <div>
+            <p className="text-[11px] mt-1">Jeptum hp medicine</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <del className="text-xs font-mono text-gray-400">$454</del>{" "}
+                <span className="font-mono text-yellow-500 font-semibold">
+                  $299
+                </span>
+              </div>
+              <p className="text-[10px] bg-red-600 px-1 py-0.5 text-white rounded-md">
+                <span>12</span>% Off
+              </p>
+            </div>
+          </div>
+          <button className="uppercase text-xs border w-full py-0.5 font-sans my-1 border-yellow-300 text-neutral-800 ">
+            Add to cart
+          </button>
+        </SwiperSlide>
+        <SwiperSlide className="bg-white rounded-sm shadow-sm p-1 my-2 mx-1">
+          <Image
+            src="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+            alt=""
+            width={200}
+            height={200}
+            className="object-cover w-full h-32 rounded-sm"
+          />
+          <div>
+            <p className="text-[11px] mt-1">Jeptum hp medicine</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <del className="text-xs font-mono text-gray-400">$454</del>{" "}
+                <span className="font-mono text-yellow-500 font-semibold">
+                  $299
+                </span>
+              </div>
+              <p className="text-[10px] bg-red-600 px-1 py-0.5 text-white rounded-md">
+                <span>12</span>% Off
+              </p>
+            </div>
+          </div>
+          <button className="uppercase text-xs border w-full py-0.5 font-sans my-1 border-yellow-300 text-neutral-800 ">
+            Add to cart
+          </button>
+        </SwiperSlide>
+        <SwiperSlide className="bg-white rounded-sm shadow-sm p-1 my-2 mx-1">
+          <Image
+            src="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+            alt=""
+            width={200}
+            height={200}
+            className="object-cover w-full h-32 rounded-sm"
+          />
+          <div>
+            <p className="text-[11px] mt-1">Jeptum hp medicine</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <del className="text-xs font-mono text-gray-400">$454</del>{" "}
+                <span className="font-mono text-yellow-500 font-semibold">
+                  $299
+                </span>
+              </div>
+              <p className="text-[10px] bg-red-600 px-1 py-0.5 text-white rounded-md">
+                <span>12</span>% Off
+              </p>
+            </div>
+          </div>
+          <button className="uppercase text-xs border w-full py-0.5 font-sans my-1 border-yellow-300 text-neutral-800 ">
+            Add to cart
+          </button>
+        </SwiperSlide>
+        <SwiperSlide className="bg-white rounded-sm shadow-sm p-1 my-2 mx-1">
+          <Image
+            src="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+            alt=""
+            width={200}
+            height={200}
+            className="object-cover w-full h-32 rounded-sm"
+          />
+          <div>
+            <p className="text-[11px] mt-1">Jeptum hp medicine</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <del className="text-xs font-mono text-gray-400">$454</del>{" "}
+                <span className="font-mono text-yellow-500 font-semibold">
+                  $299
+                </span>
+              </div>
+              <p className="text-[10px] bg-red-600 px-1 py-0.5 text-white rounded-md">
+                <span>12</span>% Off
+              </p>
+            </div>
+          </div>
+          <button className="uppercase text-xs border w-full py-0.5 font-sans my-1 border-yellow-300 text-neutral-800 ">
+            Add to cart
+          </button>
+        </SwiperSlide>
+      </Swiper>
+      <Swiper slidesPerView={2.5} spaceBetween={10} className="mySwiper my-1">
+        <SwiperSlide className="bg-white rounded-sm shadow-sm p-1 my-2 mx-1">
+          <Image
+            src="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+            alt=""
+            width={200}
+            height={200}
+            className="object-cover w-full h-32 rounded-sm"
+          />
+          <div>
+            <p className="text-[11px] mt-1">Jeptum hp medicine</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <del className="text-xs font-mono text-gray-400">$454</del>{" "}
+                <span className="font-mono text-yellow-500 font-semibold">
+                  $299
+                </span>
+              </div>
+              <p className="text-[10px] bg-red-600 px-1 py-0.5 text-white rounded-md">
+                <span>12</span>% Off
+              </p>
+            </div>
+          </div>
+          <button className="uppercase text-xs border w-full py-0.5 font-sans my-1 border-yellow-300 text-neutral-800 ">
+            Add to cart
+          </button>
+        </SwiperSlide>
+        <SwiperSlide className="bg-white rounded-sm shadow-sm p-1 my-2 mx-1">
+          <Image
+            src="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+            alt=""
+            width={200}
+            height={200}
+            className="object-cover w-full h-32 rounded-sm"
+          />
+          <div>
+            <p className="text-[11px] mt-1">Jeptum hp medicine</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <del className="text-xs font-mono text-gray-400">$454</del>{" "}
+                <span className="font-mono text-yellow-500 font-semibold">
+                  $299
+                </span>
+              </div>
+              <p className="text-[10px] bg-red-600 px-1 py-0.5 text-white rounded-md">
+                <span>12</span>% Off
+              </p>
+            </div>
+          </div>
+          <button className="uppercase text-xs border w-full py-0.5 font-sans my-1 border-yellow-300 text-neutral-800 ">
+            Add to cart
+          </button>
+        </SwiperSlide>
+        <SwiperSlide className="bg-white rounded-sm shadow-sm p-1 my-2 mx-1">
+          <Image
+            src="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+            alt=""
+            width={200}
+            height={200}
+            className="object-cover w-full h-32 rounded-sm"
+          />
+          <div>
+            <p className="text-[11px] mt-1">Jeptum hp medicine</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <del className="text-xs font-mono text-gray-400">$454</del>{" "}
+                <span className="font-mono text-yellow-500 font-semibold">
+                  $299
+                </span>
+              </div>
+              <p className="text-[10px] bg-red-600 px-1 py-0.5 text-white rounded-md">
+                <span>12</span>% Off
+              </p>
+            </div>
+          </div>
+          <button className="uppercase text-xs border w-full py-0.5 font-sans my-1 border-yellow-300 text-neutral-800 ">
+            Add to cart
+          </button>
+        </SwiperSlide>
+        <SwiperSlide className="bg-white rounded-sm shadow-sm p-1 my-2 mx-1">
+          <Image
+            src="https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+            alt=""
+            width={200}
+            height={200}
+            className="object-cover w-full h-32 rounded-sm"
+          />
+          <div>
+            <p className="text-[11px] mt-1">Jeptum hp medicine</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <del className="text-xs font-mono text-gray-400">$454</del>{" "}
+                <span className="font-mono text-yellow-500 font-semibold">
+                  $299
+                </span>
+              </div>
+              <p className="text-[10px] bg-red-600 px-1 py-0.5 text-white rounded-md">
+                <span>12</span>% Off
+              </p>
+            </div>
+          </div>
+          <button className="uppercase text-xs border w-full py-0.5 font-sans my-1 border-yellow-300 text-neutral-800 ">
+            Add to cart
+          </button>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
