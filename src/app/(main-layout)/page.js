798d5c4fb7f5f360a0f694b0bacd8bba1be02fe8
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-07-06 08:25:13 +0600

    create ourBrands section with responsive

--- src/app/(main-layout)/page.js
+++ src/app/(main-layout)/page.js
@@ -1,4 +1,5 @@
 import Banner from '@/components/Pages/Home/Banner';
+import OurBrands from '@/components/Pages/Home/OurBrands/OurBrands';
 import React from 'react';
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
         <div>
             {/* all home page components will be place here */}
             <Banner></Banner>
+            <OurBrands />
             this is home page
         </div>
     );
