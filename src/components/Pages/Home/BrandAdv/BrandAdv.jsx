ecc36ceda28067999238534897b19f3bbf51139b
ND Morsalin <morsalin056@gmail.com>
2023-07-08 15:56:58 +0600

    bug fix on nav bar and change layout of pushproduct

--- src/components/Pages/Home/BrandAdv/BrandAdv.jsx
+++ src/components/Pages/Home/BrandAdv/BrandAdv.jsx
@@ -6,10 +6,10 @@ const BrandAdv = () => {
       <div className="relative group overflow-hidden rounded-lg ">
         <Image src={bannerAdv} alt="" className="" />
         <div className="z-20  absolute p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center space-y-4">
-          <h1 className="relative left-[calc(100%+20rem)] duration-500 group-hover:left-0 text-3xl md:text-5xl font-bold text-white">
+          <h1 className="relative hidden md:block left-[calc(100%+20rem)] duration-500 group-hover:left-0 text-3xl md:text-5xl font-bold text-white">
             Brand Adv
           </h1>
-          <p className="relative right-[calc(100%+20rem)] duration-500 group-hover:right-0 text-slate-100 text-sm md:text-base">
+          <p className=" hidden md:block relative right-[calc(100%+20rem)] duration-500 group-hover:right-0 text-slate-100 text-sm md:text-base">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
             voluptatum.
           </p>
--- src/components/Pages/Home/PushProducts/PushProducts.jsx
+++ src/components/Pages/Home/PushProducts/PushProducts.jsx
@@ -5,8 +5,8 @@ const PushProducts = () => {
     <>
       <div className="container px-2 md:px-8 mx-auto mt-8">
         <h1 className="text-2xl font-bold">Push Products</h1>
-        <div className="flex items-center gap-4 md:gap-8 mt-4">
-          <div className="flex-1 flex  p-4 gap-2 shadow-lg rounded-md border">
+        <div className="flex flex-wrap items-center gap-4 md:gap-8 mt-4">
+          <div className="w-full md:flex-1  flex  p-4 gap-2 shadow-lg rounded-md border">
             <div className="">
               <h4 className="font-bold">BmcMe 3swiCode</h4>
               <h4 className="text-xl text-orange-700 font-bold">Incubator</h4>
@@ -22,21 +22,18 @@ const PushProducts = () => {
             <Image
               src={incubatorImage}
               alt="incubator"
-              width={200}
-              height={200}
+              className="w-[100px] h-[150px] md:w-[300px] md:h-[300px] "
             />
           </div>
-          <div className="md:flex-1 w-1/4 p-4  flex flex-col-reverse md:flex-row  gap-2 shadow-lg rounded-md flex-shrink border">
+          <div className="w-full md:flex-1  flex  flex-row-reverse md:flex-row p-4 gap-2 shadow-lg rounded-md border">
             <div className="">
-              <div className="hidden md:block">
-                <h4 className="text-lg font-bold">BmcMe 3swiCode</h4>
-                <h4 className="text-xl text-orange-700 font-bold">Incubator</h4>
-                <p className="text-sm text-gray-500">
-                  Lorem ipsum dolor sit amet.lorem Lorem ipsum dolor sit amet
-                  consectetur adipisicing.
-                </p>
-              </div>
-              <button className="rounded-full w-fit mx-auto block mt-4 px-4 py-2 bg-gradient-to-br hover:to-orange-600 hover:from-pink-500 from-orange-600 to-pink-500 text-white">
+              <h4 className="font-bold">BmcMe 3swiCode</h4>
+              <h4 className="text-xl text-orange-700 font-bold">Incubator</h4>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet.lorem Lorem ipsum dolor sit amet
+                consectetur adipisicing.
+              </p>
+              <button className="rounded-full w-fit mx-auto block mt-4 px-4 py-2 bg-gradient-to-br from-orange-600 to-pink-500 hover:to-orange-600 hover:from-pink-500 text-white">
                 But Now
               </button>
             </div>
@@ -44,8 +41,7 @@ const PushProducts = () => {
             <Image
               src={incubatorImage}
               alt="incubator"
-              width={200}
-              height={200}
+              className="w-[100px] h-[150px] md:w-[300px] md:h-[300px] "
             />
           </div>
         </div>
