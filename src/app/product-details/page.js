07d5ac135aebdf5af0cfd750c8358c86f5b824cd
delwerhossain <delwerhossain006@gmail.com>
2023-07-10 14:20:54 +0100

    add: product details page for all components added

--- src/app/product-details/page.js
+++ src/app/product-details/page.js
@@ -1,7 +1,16 @@
+import ImageBanner from "@/components/Pages/product-details/ImageBanner/ImageBanner";
+import ProductDescription from "@/components/Pages/product-details/ProductDescription/ProductDescription";
+import ProductInfo from "@/components/Pages/product-details/ProductInfo/ProductInfo";
+import RelatedProducts from "@/components/Pages/product-details/RelatedProducts/RelatedProducts";
+
 const page = () => {
   return (
     <div>
       <h1>products details</h1>
+      <ImageBanner />
+      <ProductInfo />
+      <ProductDescription />
+      <RelatedProducts />
     </div>
   );
 };
---
+++ src/components/Pages/product-details/ProductInfo/ProductInfo.jsx
@@ -0,0 +1,9 @@
+const ProductInfo = () => {
+    return (
+      <div className="border-8  h-32">
+        <h1>ProductInfo</h1>
+      </div>
+    );
+};
+
+export default ProductInfo;
