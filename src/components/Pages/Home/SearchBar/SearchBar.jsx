c1a2063abd4d8b52725cb998a166857328b92795
delwerhossain <delwerhossain006@gmail.com>
2023-07-10 13:26:04 +0100

    update: search bar input field optimize

--- src/components/Pages/Home/SearchBar/SearchBar.jsx
+++ src/components/Pages/Home/SearchBar/SearchBar.jsx
@@ -26,7 +26,7 @@ const SearchBar = () => {
           </div>
 
           <input
-            className="font-bold uppercase rounded-full w-full py-4 pl-4 text-gray-700 bg-gray-100 leading-tight focus:outline-slate-600  lg:text-sm text-xs"
+            className="font-bold uppercase px-3 rounded-full w-full py-4 pl-4 text-gray-700 bg-gray-100 leading-tight focus:outline-slate-600  lg:text-sm text-xs"
             type="text"
             placeholder="Search"
           />
