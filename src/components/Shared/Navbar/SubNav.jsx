69706a703b279a21f30861bbc898f34ec46746bd
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-07-15 23:08:52 +0600

    sub nab added

--- src/components/Shared/Navbar/SubNav.jsx
+++ src/components/Shared/Navbar/SubNav.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React from "react";
+import { FaBars, FaPhoneAlt, FaRegHeart, FaStore, FaRegFile } from "react-icons/fa";
 
-const SubNav = ({handleSidebarOpen}) => {
-    return (
-        <div className='h-10 w-full bg-white -mt-2 grid grid-cols-12 border-y'>
-            <div className='col-start-1 col-end-3'>
-                <button onClick={handleSidebarOpen} className='w-full h-full'>
-                category
-                </button>
-            </div>
-            <div className='col-start-3 col-end-13 flex w-full justify-between'>
-                <div>
-                    div1
-                </div>
-                <div>
-                    div2
-                </div>
-            </div>
+const SubNav = ({ handleSidebarOpen }) => {
+  return (
+    <div className="h-10 w-full bg-white -mt-2 grid grid-cols-12 border-y">
+      <div className="col-start-1 col-end-3">
+        <button
+          onClick={handleSidebarOpen}
+          className="w-full h-full text-left flex items-center gap-2 pl-4"
+        >
+          <FaBars className="w-4 h-4" />
+          <p className="font-semibold">Categories</p>
+        </button>
+      </div>
+      <div className="col-start-3 col-end-13 flex w-full justify-between">
+        <div className="flex items-center gap-5">
+          <div className="cursor-pointer flex items-center gap-1">
+            <FaPhoneAlt className="w-3 h-3" />
+            <p className="text-sm font-medium">Call To Order</p>
+          </div>
+          <div className="cursor-pointer flex items-center gap-1">
+            <FaRegHeart className="w-3 h-3" />
+            <p className="text-sm font-medium">Healthcare Product</p>
+          </div>
+          <div className="cursor-pointer flex items-center gap-1">
+            <FaStore className="w-3 h-3" />
+            <p className="text-sm font-medium">Pharmacy Register</p>
+          </div>
         </div>
-    );
+        <div className="flex items-center gap-5">
+          <div className="cursor-pointer flex items-center gap-1">
+            <p className="text-sm font-medium">Career</p>
+          </div>
+          <div className="cursor-pointer flex items-center gap-1">
+            <p className="text-sm font-medium">To quick order</p>
+          </div>
+          <div className="cursor-pointer flex items-center gap-1">
+            <FaRegFile className="w-3 h-3" />
+            <p className="text-sm font-medium">Upload Prescription</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubNav;
