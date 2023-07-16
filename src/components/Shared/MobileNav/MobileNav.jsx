eb64a1e451f4ab5404962cb8725c8bc0a58af94a
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-07-16 20:02:18 +0600

    Mobile toggle added

--- src/components/Shared/MobileNav/MobileNav.jsx
+++ src/components/Shared/MobileNav/MobileNav.jsx
@@ -1,11 +1,16 @@
 import SearchBar from "@/components/Shared/SearchBar/SearchBar";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { IconContext } from "react-icons";
 import { BiCartAdd, BiSolidOffer } from "react-icons/bi";
 import { BsChevronDown, BsPersonCircle } from "react-icons/bs";
 
 const MobileNav = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const toggleMenu = () => {
+    setIsOpenMenu(!isOpenMenu);
+  };
   return (
     <div className="">
       <div className="flex justify-between items-center mx-2 mt-3 mb-2">
@@ -54,8 +59,18 @@ const MobileNav = () => {
         </div>
       </div>
       <div>
-        <SearchBar />
+        <SearchBar toggleMenu={toggleMenu} />
       </div>
+      {/* drawer for mobile devices */}
+      {isOpenMenu && (
+        <>
+          <label
+            onClick={toggleMenu}
+            className="fixed h-screen z-[59] top-0  w-full bg-black bg-opacity-20 lg:hidden"
+          ></label>
+          <div className="fixed h-screen z-[60] top-0 left-0 w-[200px] bg-white duration-200 lg:hidden"></div>
+        </>
+      )}
     </div>
   );
 };
