a287f7a1b073744cdf4e245288020fade6412b8d
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-07-21 16:45:43 +0600

    order details and customers page done without responsive

--- src/app/(dashboard-layout)/dashboard/order-details/page.js
+++ src/app/(dashboard-layout)/dashboard/order-details/page.js
@@ -1,6 +1,5 @@
-"use client"
+import OrderedProductTable from '@/components/Pages/Dashboard/OrderDetails/OrderedProductTable';
 import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
 
 const orderDetails = {
     id: 239847,
@@ -39,14 +38,6 @@ const orderDetails = {
 }
 
 const OrderDetailsPage = () => {
-    const [subtotal, setSubtotal] = useState(0);
-    useEffect(()=>{
-        let total = 0;
-        orderDetails.products.forEach(product => {
-            total += product.price * product.quantity;
-        });
-        setSubtotal(total);
-    },[])
     return (
         <div className='w-full min-h-screen p-2'>
             <div className='h-fit  w-full bg-slate-50 rounded p-3 shadow-md'>
@@ -90,44 +81,7 @@ const OrderDetailsPage = () => {
                 </div>
             </div>
             <div className='h-fit  w-full bg-slate-50 rounded p-3 shadow-md mt-4'>
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* head */}
-                        <thead>
-                            <tr>
-                                <th>No.</th>
-                                <th>Products</th>
-                                <th className='text-right'>Quantity</th>
-                                <th className='text-right'>Price</th>
-                                <th className='text-right'>Amount</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                orderDetails.products.map((product, index) => <>
-                                    <tr>
-                                        <th className='font-medium'>{index + 1}</th>
-                                        <td><div>
-                                            <p className='text-base font-semibold text-gray-600'>{product.name}</p>
-                                            <p className='text-sm text-slate-500'>{product.model}</p>
-                                        </div></td>
-                                        <td className='text-right'>{product.quantity}</td>
-                                        <td className='text-right'>${product.price}</td>
-                                        <td className='text-right'>${product.price * product.quantity}</td>
-                                    </tr>
-                                </>)
-                            }
-                        </tbody>
-                    </table>
-                    <div className='w-full h-fit flex justify-end'>
-                        <div className='text-sm font-medium text-gray-500 text-right'>
-                            <p>Subtotal: ${subtotal}</p>
-                            <p>Shipping Cost: ${20}</p>
-                            <hr className='my-1'></hr>
-                            <p className=' font-semibold'>Total: ${subtotal + 20}</p>
-                        </div>
-                    </div>
-                </div>
+                <OrderedProductTable orderDetails={orderDetails} />
             </div>
         </div>
     );
