2eb9685156b3de5e1f1b7972475f28e18ab906c5
Syed Mir Habib <mirakik.habib@gmail.com>
2023-07-27 12:58:07 +0600

    dashboard link is added - homepage for easy access

--- src/components/Shared/Navbar/SubNav.jsx
+++ src/components/Shared/Navbar/SubNav.jsx
@@ -3,19 +3,31 @@ import { BiPhoneCall } from "react-icons/bi";
 import { BsShopWindow } from "react-icons/bs";
 import { FaBars, FaRegHeart } from "react-icons/fa";
 import { LiaFilePrescriptionSolid } from "react-icons/lia";
+import Link from "next/link";
 const SubNav = ({ handleSidebarOpen }) => {
   return (
     <div className="h-12 w-full -mt-2 grid grid-cols-12 border-y bg-slate-100 lg:px-3">
       <div className="col-start-1 col-end-3">
         <button
           onClick={handleSidebarOpen}
-          className="w-full h-full text-left flex items-center gap-2 pl-4">
+          className="w-full h-full text-left flex items-center gap-2 pl-4"
+        >
           <FaBars className="w-4 h-4" />
           <p className="font-semibold">Categories</p>
         </button>
       </div>
+
       <div className="col-start-3 col-end-13 flex w-full justify-between">
         <div className="flex items-center gap-7">
+          <Link href="/dashboard">
+            <div className="cursor-pointer flex items-center gap-1 hover:outline outline-teal-400 rounded-sm p-1">
+              <BiPhoneCall className="w-3 h-3" />
+              <p className="text-sm text-green-600 font-extrabold ">
+                DashBoard
+              </p>
+            </div>
+          </Link>
+
           <div className="cursor-pointer flex items-center gap-1 hover:outline outline-teal-400 rounded-sm p-1">
             <BiPhoneCall className="w-3 h-3" />
             <p className="text-sm font-medium">Call To Order</p>
@@ -29,6 +41,7 @@ const SubNav = ({ handleSidebarOpen }) => {
             <p className="text-sm font-medium">Pharmacy Register</p>
           </div>
         </div>
+
         <div className="flex items-center gap-7">
           <div className="cursor-pointer flex items-center gap-1 hover:outline outline-teal-400 rounded-sm p-1">
             <p className="text-sm font-medium">Career</p>
