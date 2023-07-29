d295c5e5c4b84e4812d65cda86b982fe2cf5e9e6
Eshrak <eshrakg62@gmail.com>
2023-07-29 16:22:14 +0600

    Change the code format and add some style to border and hover

--- src/app/(main-layout)/user-dashboard/page.jsx
+++ src/app/(main-layout)/user-dashboard/page.jsx
@@ -1,15 +1,51 @@
- 
-
 import userimg from "../../../assests/userprofile.png";
 import Image from "next/image";
-import { FaShoppingCart } from "react-icons/fa";
-import './dashbord.css'
+import { FaBookmark, FaShoppingCart, FaUserAlt } from "react-icons/fa";
+import "./dashbord.css";
+import { MdLock } from "react-icons/md";
+import { AiFillStar, AiOutlineTransaction } from "react-icons/ai";
 
 const UserDashBoard = () => {
+  const cardData = [
+    {
+      icon: <FaShoppingCart color="blue" />,
+      title: "Orders",
+    },
+    {
+      icon: <FaUserAlt color="blue" />,
+      title: "Edit Profile",
+    },
+    {
+      icon: <MdLock color="blue" />,
+      title: "Password ",
+    },
+    {
+      icon: <FaBookmark color="blue" />, //IoMdBookmarks
+      title: "Addresses",
+    },
+    {
+      icon: <FaShoppingCart color="blue" />, // GiEternalLove
+      title: "Whish list",
+    },
+    {
+      icon: <FaBookmark color="blue" />, // FaComputer
+      title: "Saved Pc",
+    },
+    {
+      icon: <AiFillStar color="blue" />,
+      title: "Start Points",
+    },
+    {
+      icon: <AiOutlineTransaction color="blue" />,
+      title: "Transactions",
+    },
+    // Add more card data objects here
+  ];
+
   return (
     <div>
       <div className="mx-5 flex flex-col ">
-        <div className="flex flex-col md:flex-row justify-between  items-center ">
+        <div className="flex flex-col md:flex-row justify-between items-center ">
           <div className="flex items-center">
             <Image src={userimg} alt="" className="" />
             <div className="flex flex-col">
@@ -30,40 +66,18 @@ const UserDashBoard = () => {
         </div>
       </div>
       <div className="grid grid-cols-3 md:grid-cols-4 gap-5 m-4">
-        {/* Add "group" to each card to enable hover styles */}
-        <div className="group py-10 px-5 flex flex-col items-center user-card">
-          <FaShoppingCart className="mb-2"></FaShoppingCart>
-          <p>Orders</p>
-        </div>
-        <div className="group py-10 px-5 flex flex-col items-center user-card">
-          <FaShoppingCart className="mb-2"></FaShoppingCart>
-          <p>Orders</p>
-        </div>
-        <div className="group py-10 px-5 flex flex-col items-center user-card">
-          <FaShoppingCart className="mb-2"></FaShoppingCart>
-          <p>Orders</p>
-        </div>
-        <div className="group py-10 px-5 flex flex-col items-center user-card">
-          <FaShoppingCart className="mb-2"></FaShoppingCart>
-          <p>Orders</p>
-        </div>
-        <div className="group py-10 px-5 flex flex-col items-center user-card">
-          <FaShoppingCart className="mb-2"></FaShoppingCart>
-          <p>Orders</p>
-        </div>
-        <div className="group py-10 px-5 flex flex-col items-center user-card">
-          <FaShoppingCart className="mb-2"></FaShoppingCart>
-          <p>Orders</p>
-        </div>
-        <div className="group py-10 px-5 flex flex-col items-center user-card">
-          <FaShoppingCart className="mb-2"></FaShoppingCart>
-          <p>Orders</p>
-        </div>
-        <div className="group py-10 px-5 flex flex-col items-center user-card">
-          <FaShoppingCart className="mb-2"></FaShoppingCart>
-          <p>Orders</p>
-        </div>
-        {/* Add more cards here */}
+        {cardData.map((card, index) => (
+       <div
+       key={index}
+       className="group py-2 px-2 md:py-10 md:px-5 flex flex-col items-center justify-center user-card border border-transparent hover:border-blue-800 shadow-lg transition-all duration-400"
+     >
+       <span className="w-11 h-11 rounded-full bg-[#EBEDF8] flex items-center justify-center mb-2">
+         {card.icon}
+       </span>
+       <p className="text-sm md:text-lg">{card.title}</p>
+     </div>
+     
+        ))}
       </div>
     </div>
   );
