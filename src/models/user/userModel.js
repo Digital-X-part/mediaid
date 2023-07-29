af016dec70d6eeef972ad6db797a9c4f85a56de2
delwerhossain <delwerhossain006@gmail.com>
2023-07-29 16:05:35 +0100

    update : improve

--- src/models/user/userModel.js
+++ src/models/user/userModel.js
@@ -18,71 +18,77 @@ const addressSchema = new Schema({
   },
 });
 
-const userSchema = new Schema({
-  fullName: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    photo: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 255,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    rewardPoints: {
+      type: Number,
+      default: 0,
+    },
+    addresses: [addressSchema], // Use an array for multiple addresses
   },
-  phone: {
-    type: Number,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  photo: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 255,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-  rewardPoints: {
-    type: Number,
-    default: 0,
-  },
-  addresses: [addressSchema], // Use an array for multiple addresses
-});
+  { versionKey: false }
+);
 
 export const User = models?.User || model("User", userSchema);
 
 // update for user  schema
 
-const updateSchema = new Schema({
-  fullName: {
-    type: String,
+const updateSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    photo: {
+      type: String,
+    },
+    password: {
+      type: String,
+      minlength: 8,
+      maxlength: 255,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    rewardPoints: {
+      type: Number,
+      default: 0,
+    },
+    addresses: [addressSchema], // Use an array for multiple addresses
   },
-  phone: {
-    type: Number,
-  },
-  email: {
-    type: String,
-    unique: true,
-  },
-  photo: {
-    type: String,
-  },
-  password: {
-    type: String,
-    minlength: 8,
-    maxlength: 255,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-  rewardPoints: {
-    type: Number,
-    default: 0,
-  },
-  addresses: [addressSchema], // Use an array for multiple addresses
-});
+  { versionKey: false }
+);
 
 export const UpdateUser = models?.User || model("User", updateSchema);
