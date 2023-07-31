7afc8d411c9cb7049194706c8deee3c818a08349
Syed Mir Habib <mirakik.habib@gmail.com>
2023-07-31 09:42:02 +0600

    changes: footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -9,87 +9,112 @@ import payWith from "@/assests/Available-Payment-Methods-550x461.bk-2.png";
 
 const Footer = () => {
   return (
-    <div className="py-4 px-3 md:px-7 sm:p-4 md:py-8 w-full bg-slate-800 mt-10 flex items-center justify-center relative">
-      <div className=" w-full grid grid-cols-2 md:grid-cols-4 gap-y-5 gap-x-2 pb-[28px]">
-        <div>
-          <h1 className="text-white text-sm md:text-xl font-semibold uppercase">
+    <div className="py-8 px-4 md:px-12 lg:px-24 bg-gray-800">
+      <div className="max-w-6xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-y-8 gap-x-6">
+        <div className="text-white">
+          <h1 className="text-lg md:text-xl font-semibold uppercase mb-4">
             Information
           </h1>
-          <ul className="text-slate-200 text-xs md:text-sm font-normal space-y-1 mt-2">
+          <ul className="text-sm md:text-base font-normal space-y-2">
             <li>
-              <a href="">Career</a>
+              <a href="#" className="hover:text-gray-300">
+                Career
+              </a>
             </li>
             <li>
-              <a href="">Privacy Policy</a>
+              <a href="#" className="hover:text-gray-300">
+                Privacy Policy
+              </a>
             </li>
             <li>
-              <a href="">Terms & Conditions</a>
+              <a href="#" className="hover:text-gray-300">
+                Terms & Conditions
+              </a>
             </li>
             <li>
-              <a href="">Return & Refund policy</a>
+              <a href="#" className="hover:text-gray-300">
+                Return & Refund Policy
+              </a>
             </li>
           </ul>
         </div>
-        <div>
-          <h1 className="text-white text-sm md:text-xl font-semibold uppercase">
-            Useful Link
+        <div className="text-white">
+          <h1 className="text-lg md:text-xl font-semibold uppercase mb-4">
+            Useful Links
           </h1>
-          <ul className="text-slate-200 text-xs md:text-sm font-normal space-y-1 mt-2">
+          <ul className="text-sm md:text-base font-normal space-y-2">
             <li>
-              <a href="">FAQ</a>
+              <a href="#" className="hover:text-gray-300">
+                FAQ
+              </a>
             </li>
             <li>
-              <a href="">Shop</a>
+              <a href="#" className="hover:text-gray-300">
+                Shop
+              </a>
             </li>
             <li>
-              <a href="">Special Offers</a>
+              <a href="#" className="hover:text-gray-300">
+                Special Offers
+              </a>
             </li>
             <li>
-              <a href="">My Account</a>
+              <a href="#" className="hover:text-gray-300">
+                My Account
+              </a>
             </li>
           </ul>
         </div>
-        <div>
-          <h1 className="text-white text-sm md:text-xl font-semibold uppercase">
+        <div className="text-white">
+          <h1 className="text-lg md:text-xl font-semibold uppercase mb-4">
             Contact Us
           </h1>
-          <ul className="text-slate-200 text-xs md:text-sm font-normal space-y-1 mt-2">
+          <ul className="text-sm md:text-base font-normal space-y-2">
             <li>
-              <a href="">Address: Bonani, Dhaka-1100, Bangladesh</a>
+              Address: Bonani, Dhaka-1100, Bangladesh
             </li>
             <li>
-              <a href="">Hot Line: 16226</a>
+              Hot Line: 16226
             </li>
             <li>
-              <a href="">Email: mediaid@gmail.com</a>
+              Email: mediaid@gmail.com
             </li>
             <li>
               <div>
-                <h1>Social Links</h1>
-                <div className="flex items-center gap-2 mt-2">
-                  <FaFacebookSquare className="text-xs text-blue-400 hover:text-slate-200 cursor-pointer" />
-                  <FaInstagram className="text-xs text-blue-400 hover:text-slate-200 cursor-pointer" />
-                  <FaTwitterSquare className="text-xs text-blue-400 hover:text-slate-200 cursor-pointer" />
-                  <FaLinkedin className="text-xs text-blue-400 hover:text-slate-200 cursor-pointer" />
+                <h1 className="font-semibold mb-2">Social Links</h1>
+                <div className="flex items-center gap-2">
+                  <a href="#" className="text-blue-400 hover:text-gray-300">
+                    <FaFacebookSquare className="text-xl" />
+                  </a>
+                  <a href="#" className="text-blue-400 hover:text-gray-300">
+                    <FaInstagram className="text-xl" />
+                  </a>
+                  <a href="#" className="text-blue-400 hover:text-gray-300">
+                    <FaTwitterSquare className="text-xl" />
+                  </a>
+                  <a href="#" className="text-blue-400 hover:text-gray-300">
+                    <FaLinkedin className="text-xl" />
+                  </a>
                 </div>
               </div>
             </li>
           </ul>
         </div>
         <div>
-          <h1 className="text-white text-sm md:text-xl font-semibold uppercase">
+          <h1 className="text-lg md:text-xl font-semibold uppercase mb-4">
             Pay With
           </h1>
           <Image
             src={payWith}
             alt="paywith"
-            fill={false}
-            className="mt-2 md:w-[80%]"
-          ></Image>
+            width={440}
+            height={368}
+            className="w-full"
+          />
         </div>
       </div>
-      <p className="absolute bottom-3 text-xs text-white">
-        &copy; 2023. All right reserved by MediAid
+      <p className="text-center text-sm md:text-base text-white mt-8">
+        &copy; {new Date().getFullYear()}. All rights reserved by MediAid
       </p>
     </div>
   );
