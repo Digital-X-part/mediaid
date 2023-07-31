b4381540f91fa0c13ba3c6a2c27ed57e3c106452
Syed Mir Habib <mirakik.habib@gmail.com>
2023-07-31 11:18:29 +0600

    Revamp OurBrands: Beautify and Improve

--- src/components/Pages/Home/OurBrands/OurBrands.jsx
+++ src/components/Pages/Home/OurBrands/OurBrands.jsx
@@ -1,4 +1,3 @@
-"use client";
 import brandLogo from "@/data/brands.json";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
@@ -8,18 +7,18 @@ const OurBrands = () => {
     <div className="my-6">
       <h4 className="font-bold text-2xl ml-1">Our Brands</h4>
       <div className="mt-4">
-        <Marquee>
+        <Marquee speed={40} pauseOnHover={true}>
           {brandLogo.map((brand) => (
             <div
               key={brand.id}
-              className="w-28 h-24 lg:w-32 lg:h-28 p-2 shadow-md hover:scale-90 duration-200 ml-4 border-2"
+              className="w-28 h-24 lg:w-32 lg:h-28 p-2 shadow-md hover:scale-105 duration-200 ml-4 border border-gray-300 rounded-lg bg-white"
             >
               <Image
                 src={brand.brand_logo}
                 width={500}
                 height={100}
-                alt="kd"
-                className="object-cover w-full h-full "
+                alt={brand.name}
+                className="object-contain w-full h-full"
               />
             </div>
           ))}
