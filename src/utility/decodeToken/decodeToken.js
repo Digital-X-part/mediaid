cc8fee7aa49f0b55e604427d0a8fe042029505fb
NDmorsalin <morsalin056@gmail.com>
2023-08-03 22:08:24 +0600

    decode token from middleware

--- src/utility/decodeToken/decodeToken.js
+++ src/utility/decodeToken/decodeToken.js
@@ -1,12 +1,6 @@
 import jwt from "jsonwebtoken";
 const decodeToken = async (token) => {
   try {
-      let token = request.cookies.get('token')
-      
-      console.log('middleware',token);
-      if(token){
-            decodeToken(token.value);
-      }
     const decodedToken = jwt.decode(token, process.env.TOKEN_SECRET);
     console.log({ decodedToken });
     return decodedToken;
