27ccc3586eee6294f5d091b3527d1376cfaa667c
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-08-04 14:46:44 +0600

    some change in product details page

--- src/components/Pages/product-details/ImageBanner/ImageBanner.jsx
+++ src/components/Pages/product-details/ImageBanner/ImageBanner.jsx
@@ -4,7 +4,7 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
-import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { FreeMode, Thumbs } from "swiper/modules";
 import "./styles.css";
 import Image from "next/image";
 import { useState } from "react";
@@ -57,16 +57,13 @@ const ImageBanner = () => {
     <div className="mb-12 h-fit w-full ">
       <Swiper
         style={{
-          "--swiper-navigation-color": "#A0AEC0",
           "--swiper-pagination-color": "#A0AEC0",
         }}
         loop={true}
         spaceBetween={0}
-        navigation={true}
         thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >
+        modules={[FreeMode, Thumbs]}
+        className="mySwiper2">
         {productImages.map((img) => (
           <SwiperSlide key={img.id}>
             <Image height={600} width={1100} alt={img.name} src={img.url} />
@@ -83,14 +80,13 @@ const ImageBanner = () => {
           slidesPerView={8}
           freeMode={true}
           watchSlidesProgress={true}
-          modules={[FreeMode, Navigation, Thumbs]}
-          className="mySwiper "
-        >
-            {productImages.map((img) => (
-          <SwiperSlide key={img.id}>
-            <Image height={80} width={200} alt={img.name} src={img.url} />
-          </SwiperSlide>
-        ))}
+          modules={[FreeMode, Thumbs]}
+          className="mySwiper ">
+          {productImages.map((img) => (
+            <SwiperSlide key={img.id}>
+              <Image height={80} width={200} alt={img.name} src={img.url} />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
