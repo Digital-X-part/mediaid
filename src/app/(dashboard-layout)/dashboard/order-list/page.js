4aafb9801763381fa75de062c3f89ba9ab81bc0c
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-08-07 15:23:43 +0600

    order list resolve conflict

--- src/app/(dashboard-layout)/dashboard/order-list/page.js
+++ src/app/(dashboard-layout)/dashboard/order-list/page.js
@@ -16,27 +16,7 @@ const OrderList = () => {
         </ul>
       </div>
       <div className="overflow-x-auto">
-<<<<<<< HEAD
-        <table className="table table-zebra ">
-          <thead className="bg-slate-200">
-            <tr>
-              <th>Order</th>
-              <th>Date</th>
-              <th>Order ID</th>
-              <th>Transaction ID</th>
-              <th>Ship To</th>
-              <th>Status</th>
-              <th>Amount</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <OrderListTable />
-          </tbody>
-        </table>
-=======
         <OrderTable />
->>>>>>> 46256497172a36fc1bf6041fe634cfe3b2ffe435
       </div>
     </div>
   );
